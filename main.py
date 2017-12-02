19c8074687aabdb7a0473fc45d04668bcdde0d91
n644dc <n644dc@gmail.com>
2017-12-02 10:09:55 -0700

    Idea project and such

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-#CREDIT: https://www.cryptocoinsnews.com/block-parser-how-read-bitcoin-block-chain/
+# CREDIT: https://www.cryptocoinsnews.com/block-parser-how-read-bitcoin-block-chain/
 
 import sys
 from block import Block, BlockHeader
 
+
 def parse(blockchain):
-        print('print Parsing Block Chain')
-        counter = 0
-        while True:
-                print(counter)
-                block = Block(blockchain)
-                block.toString()
-                counter+=1
+    print('print Parsing Block Chain')
+    counter = 0
+    while True:
+        print(counter)
+        block = Block(blockchain)
+        block.toString()
+        counter += 1
+
 
 def main():
-        if len(sys.argv) < 2:
-                print('Usage: blockparser.py filename')
-        else:
-                with open(sys.argv[1], 'rb') as blockchain:
-                        parse(blockchain)
+    if len(sys.argv) < 2:
+        print('Usage: blockparser.py filename')
+    else:
+        with open(sys.argv[1], 'rb') as blockchain:
+            parse(blockchain)
+
 
 if __name__ == '__main__':
-        main()
+    main()
